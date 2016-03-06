39bbd3a14be5b129e598be3bf6c734b825f080a9
soilman <earthefficientaccess@gmail.com>
2016-03-06 13:57:24 -0500

    group all_reports by project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
+  has_many :daily_reports
   has_many :manifest_reports
   has_many :trucks, through: :manifest_reports
   has_many :facilities, through: :manifest_reports
